8327509d4595e0bccdf5d58c09acfb2d6fc03eee
maxnevans <maxnevans@gmail.com>
2020-06-20 18:30:32 +0300

    service startup mode 'console' implemented

--- NewYearGarlandService/src/Main.cpp
+++ NewYearGarlandService/src/Main.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 
 #include "GarlandService.h"
+#include "GarlandApp.h"
+#include "Logger.h"
 
 std::wstring getPathToCurrentExecutable()
 {
@@ -10,6 +12,26 @@ std::wstring getPathToCurrentExecutable()
 
     return path;
 }
+
+Event stopEvent = {};
+
+BOOL WINAPI consoleHandler(DWORD signal) {
+
+    if (signal == CTRL_C_EVENT)
+    {
+        stopEvent.emmit();
+        std::wcout << L"Stopping service..." << L"\n";
+    }
+
+    return TRUE;
+}
+
+void fakeReportingStopFunction(DWORD milliseconds)
+{
+    std::wcout << L"Service reported that it will stop in " << milliseconds << L"\n";
+}
+
+
 
 int wmain(int argc, wchar_t* argv[])
 {
@@ -47,6 +69,40 @@ int wmain(int argc, wchar_t* argv[])
             std::wcout << L"Service found.\n";
             service.start();
             std::wcout << L"Service successfully started!\n";
+            return 0;
+        }
+
+        if (lstrcmpi(argv[1], L"--mode") == 0 && lstrcmpi(argv[2], L"console") == 0)
+        {
+            if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
+                printf("\nERROR: Could not set control handler");
+                return 1;
+            }
+
+            std::wcout << L"Starting service from console...\n";
+
+            wchar_t szPath[MAX_PATH];
+            if (!GetModuleFileName(NULL, szPath, MAX_PATH))
+            {
+                lstrcpy(szPath, L"C:\\logs");
+            }
+            PathRemoveFileSpec(szPath);
+            PathAppend(szPath, L"logs");
+
+
+            Logger logger(szPath, true);
+            logger.start();
+            std::wcout << L"Logger successfully initialized.\n";
+            GarlandApp app(logger);
+            std::wcout << L"Application successfully initialized.\n";
+
+            std::wcout << L"Starting app.main.\n";
+            app.main(stopEvent, fakeReportingStopFunction);
+            std::wcout << L"app.main stopped.\n";
+
+            logger.stop();
+            std::wcout << L"logger stopped.\n";
+
             return 0;
         }
 
